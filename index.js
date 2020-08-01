c4236d40e0d12d8c00cd31ebef7b936688b18154
Yuji Ito <u@110chang.com>
2020-08-01 13:35:26 +0900

    Using on end handler

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const pathToRepo = path.resolve('../mammoth2/.git')
 
 nodegit.Repository.open(pathToRepo)
   .then(function(repo) {
-    return repo.getBranchCommit('develop');
+    return repo.getBranchCommit('develop')
   })
   .then(function(firstCommit){
-    // History returns an event.
-    var history = firstCommit.history(nodegit.Revwalk.SORT.TIME);
+    const history = firstCommit.history(nodegit.Revwalk.SORT.TIME)
 
-    // History emits "commit" event for each commit in the branch's history
-    history.on("commit", function(commit) {
-      console.log("commit " + commit.sha());
-      console.log("Author:", commit.author().name() +
-        " <" + commit.author().email() + ">");
-      console.log("Date:", commit.date());
-      console.log("\n    " + commit.message());
-    });
+    history.on('end', function(commits) {
+      // Use commits
+      console.log(commits.length)
+      commits.forEach(commit => {
+        console.log(`Commit: ${commit.sha()}`)
+        console.log(`Author: ${commit.author().name()} ${commit.author().email()}`)
+        console.log(`Date: ${commit.date()}`)
+      })
+    })
 
-    // Don't forget to call `start()`!
-    history.start();
+    history.start()
   })
-  // .done();
