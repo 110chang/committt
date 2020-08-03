ea0dad1f9bd5d7ac86c4d2e58c1a574d4841f7ee
Yuji Ito <u@110chang.com>
2020-08-03 23:05:25 +0900

    Rewrite with async/await

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 const path = require('path')
 const nodegit = require('nodegit')
-
-// commanderモジュールをprogramオブジェクトとしてインポートする
-const program = require("commander")
-// コマンドライン引数をパースする
+const program = require('commander')
 program.parse(process.argv)
 
 const pathToRepo = path.resolve(program.args[0])
 
-nodegit.Repository.open(pathToRepo)
-  .then(function(repo) {
-    return repo.getBranchCommit('develop')
-  })
-  .then(function(firstCommit){
-    const history = firstCommit.history(nodegit.Revwalk.SORT.TIME)
-
-    history.on('end', function(commits) {
-      // Use commits
-      console.log(commits.length)
-      commits.forEach(commit => {
-        console.log(`Commit: ${commit.sha()}`)
-        console.log(`Author: ${commit.author().name()} ${commit.author().email()}`)
-        console.log(`Date: ${commit.date()}`)
-      })
-    })
-
+async function main() {
+  const repo = await nodegit.Repository.open(pathToRepo)
+  const firstCommit = await repo.getBranchCommit('develop')
+  const history = firstCommit.history(nodegit.Revwalk.SORT.TIME)
+  const commits = await new Promise((resolve) => {
+    history.on('end', c => resolve(c))
     history.start()
   })
+  console.log(commits.length)
+}
+
+main()
+
 // ref: https://qiita.com/highwide/items/236ab304e74a53cd3854
