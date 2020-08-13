a135ba05065f991ca7a4aca720c8f5260a4abbf9
Yuji Ito <u@110chang.com>
2020-08-13 16:59:21 +0900

    Enable to collect user's commits

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 const path = require('path')
 const nodegit = require('nodegit')
 const program = require('commander')
-program.parse(process.argv)
+program
+  .version('0.0.1')
+  .usage('[options] <file ...>')
+  .option('-a --author <value>', 'Author Name Or Email')
+  .requiredOption('-p --path <path>', 'Project Path')
+  .parse(process.argv)
 
-const pathToRepo = path.resolve(program.args[0])
+const authorNameOrEmail = program.author
+const pathToRepo = path.resolve(program.path)
+console.log('--author:', authorNameOrEmail)
+console.log('--path:', pathToRepo)
+
+async function getDefaultEmail() {
+  const defaultConfig = await nodegit.Config.openDefault()
+  return await defaultConfig.getStringBuf('user.email')
+}
 
 async function main() {
+  let author = await getDefaultEmail()
+  if (authorNameOrEmail) {
+    author = authorNameOrEmail
+  }
+  console.log('collect commit by user:', author, 'from', pathToRepo)
+  let data = []
   const repo = await nodegit.Repository.open(pathToRepo)
   const firstCommit = await repo.getBranchCommit('develop')
   const history = firstCommit.history(nodegit.Revwalk.SORT.TIME)
   const commits = await new Promise((resolve) => {
-    history.on('end', c => resolve(c))
+    history.on('commit', async (commit) => {
+      const name = commit.author().name()
+      const email = commit.author().email()
+      if (author !== name && author !== email) return
+      data.push({
+        date: commit.date(),
+        sha: commit.sha(),
+        name,
+        email,
+      })
+    })
+    history.on('end', commits => resolve(commits))
     history.start()
   })
-  console.log(commits.length)
+
+  console.log(data.length, 'of', commits.length, 'commits are found')
 }
 
 main()
