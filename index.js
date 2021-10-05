f3de0cdfdc59be59ae82ac2aca5a69176c58646e
Yuji Ito <u@110chang.com>
2021-10-05 17:52:54 +0900

    fixed options

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ program
   .version(require('./package.json').version)
   .option('-u, --user <value>', 'Author Name Or Email')
   .requiredOption('-p, --path <path>', 'Project Path')
-  .option('-d, --date <date>', 'Target month like `2020/01`')
-  .option('-t, --target <string>', 'Target duration enum [`month`, `day`]')
+  .option('-d, --date <date>', 'ISO 8601 format date like `2020-01`')
+  // .option('-t, --target <string>', 'Target duration enum [`month`, `day`]')
   .parse()
 
 const options = program.opts();
 
 async function main() {
   const author = options.user ? options.user : await Utils.getAuthor()
-  const { from, to } = Utils.createDuration(options.target)
+  const { from, to } = Utils.createDuration(options.date)
   const allCommits = await Utils.getAllCommits(path.resolve(options.path))
   const commits = Utils.filterCommits(allCommits, author, from, to)
 
-  Output.outputFoundMessage(commits.length, allCommits.length, options.target)
+  Output.outputFoundMessage(commits.length, allCommits.length, options.date)
   Output.outputBlankLine()
   Output.outputTimeTable(Utils.createTimeTableFrom(commits))
   Output.outputBlankLine()
